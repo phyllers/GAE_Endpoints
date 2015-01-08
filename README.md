7eeb840f9297de7042762137d61afc48469cdc13
phyllers <plee@systemsbiology.org>
2015-01-08 09:45:29 -0800

    Removed unused endpoints (greetings), updated readme, moved to isb-cgc project

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Api Explorer [Here](https://striking-berm-771.appspot.com/_ah/api/explorer)
 TODO:
 - Modify clients to use authorized endpoints
 
+Endpoints:
+
+- /fake_data : returns a test json object
+- /rand_int : returns a random int from 1 to 1000
+- /fake_treegraph_data : returns a fake tree of data to drive sample tree graph on demo site
+- /fmdata : returns all sample feature matrix data that was given to us (10054 samples).
+- /fmdata?{parameter}={value} : returns a filtered list of feature matrix data where the parameter=value for each record. If multiple parameters are given, they will be ANDed together.
+- /fmdata/{sample_id} : given a sample id (e.g. TCGA-OR-A5J1-01), returns all feature matrix data we have for it.
--- api.py
+++ api.py
@@ -9,36 +9,26 @@ from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
-package = 'hello'
-class Greeting(messages.Message):
-    message = messages.StringField(1)
-
-class GreetingCollection(messages.Message):
-    items = messages.MessageField(Greeting, 1, repeated=True)
-
-STORED_GREETINGS = GreetingCollection(items=[
-    Greeting(message='Hello World!'),
-    Greeting(message='Goodbye Cruel World!'),
-])
-
-class ReturnJSON(messages.Message):
-    msg = messages.StringField(1)
+package = 'isb-cgc-api'
 
 def rand():
     return randint(1, 1000)
 
+# Database connection
 env = os.getenv('SERVER_SOFTWARE')
 if env and env.startswith('Google App Engine/') or os.getenv('SETTINGS_MODE') == 'prod':
     # Connecting from App Engine
     db = MySQLdb.connect(
-        unix_socket='/cloudsql/striking-berm-771:django-test',
-        db='pong',
+        unix_socket='/cloudsql/isb-cgc:demo01',
+        db='demo',
         user='root',
     )
 else:
-    # Connecting from an external network.
-    # Make sure your network is whitelisted
+    # Connecting to localhost
     db = MySQLdb.connect(host='127.0.0.1', port=3306, db='test', user='root')
+
+class ReturnJSON(messages.Message):
+    msg = messages.StringField(1)
 
 class DBItem(messages.Message):
     id = messages.IntegerField(1)
@@ -114,52 +104,6 @@ class FMItemList(messages.Message):
 @endpoints.api(name='gae_endpoints', version='v1',)
 class GAE_Endpoints_API(remote.Service):
 
-    MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(
-        Greeting,
-        times=messages.IntegerField(2, variant=messages.Variant.INT32,
-                                    required=True))
-    @endpoints.method(MULTIPLY_METHOD_RESOURCE, Greeting,
-                      path='hellogreeting/{times}', http_method='POST',
-                      name='greetings.mulitply')
-    def greetings_multiply(self, request):
-        return Greeting(message=request.message * request.times)
-
-    @endpoints.method(message_types.VoidMessage, DBItemList,
-                      path='hellogreeting', http_method='GET',
-                      name='greetings.listGreeting',)
-    def greetings_list(self, unused_request):
-        cursor = db.cursor()
-        cursor.execute('SELECT id, content, date FROM testapp_greeting;')
-        data = []
-        for row in cursor.fetchall():
-            data.append(DBItem(id=row[0],
-                               content=row[1],
-                               date=row[2].isoformat()))
-
-        return DBItemList(items=data)
-
-    ID_RESOURCE = endpoints.ResourceContainer(
-        message_types.VoidMessage,
-        id=messages.IntegerField(1, variant=messages.Variant.INT32))
-    @endpoints.method(ID_RESOURCE, DBItem,
-                      path='hellogreeting/{id}', http_method='GET',
-                      name='greetings.getGreeting')
-    def greeting_get(self, request):
-        try:
-            cursor = db.cursor()
-            cursor.execute('SELECT id, content, date FROM testapp_greeting where id=%s;' % request.id)
-            row = cursor.fetchone()
-            return DBItem(id=row[0], content=row[1], date=row[2].isoformat())
-        except (IndexError, TypeError):
-            raise endpoints.NotFoundException('Greeting %s not found.' % (request.id,))
-
-    @endpoints.method(message_types.VoidMessage, Greeting,
-                      path='hellogreetings/authed', http_method='POST',
-                      name='greetings.authed',)
-    def greeting_authed(self, request):
-        current_user = endpoints.get_current_user()
-        email = (current_user.email() if current_user is not None else 'Anonymous')
-        return Greeting(message='Hello %s' % (email,))
 
     @endpoints.method(message_types.VoidMessage, ReturnJSON,
                       path='fake_data', http_method='GET',
